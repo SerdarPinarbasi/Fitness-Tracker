e63ca0b63cf541193bddfce12e5a8d8081bec72b
Serdar PINABAŞI <serdarpinarbasi@hotmail.com>
2026-09-16 20:22:01 +0300

    Add PWA tracker
    files uploaded

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='ul8-pwa-v1'; const ASSETS=['./upper_lower_8_hafta_tracker_pwa.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
