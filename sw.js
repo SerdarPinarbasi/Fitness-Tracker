1502fcd8a0ab46b53c2f986b796f987353bd80b6
Serdar PINABAŞI <serdarpinarbasi@hotmail.com>
2026-09-16 21:49:46 +0300

    New version
    New version added

--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
-const CACHE='ul8-pwa-v1'; const ASSETS=['./upper_lower_8_hafta_tracker_pwa.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "blok8w-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Ağ öncelikli, çevrimdışıysa önbellek. Veri zaten localStorage'da, sadece dosyalar önbelleklenir.
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
